ac78312d4a79234f22716e0b92741f07845405a2
‪Ebrahim Amgad <ebrahimamgad110@gmail.com>
2025-09-28 00:11:20 +0300

    Make header background color dynamic

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const header = document.querySelector('header');
+
 const nav = document.querySelector('nav');
 
 const bars = nav.querySelector('.bars');
@@ -9,6 +11,35 @@ const middleBar = bars.querySelector(':nth-child(2)');
 document.querySelector('footer span:nth-of-type(1)').textContent = new Date().getFullYear();
 
 let isOpened = false;
+
+const sections = {
+	features: {
+		color: "#fff"
+	},
+	services: {
+		color: "#f6f6f6"
+	},
+	portfolio: {
+		color: "#fff"
+	},
+	about: {
+		color: "#f6f6f6"
+	}
+};
+
+window.addEventListener('load', _ => {
+	for (section in sections)
+		sections[section].offsetTop = document.querySelector(`.${section}`).offsetTop;
+});
+
+window.addEventListener('scroll', _ => {
+	const scrollYValue = scrollY + 64;
+	for (section in sections) {
+		const { offsetTop, color } = sections[section]
+		if (scrollYValue >= offsetTop)
+			header.style.backgroundColor = color;
+	}
+});
 
 nav.addEventListener('click', (e) => {
 	e.stopPropagation()
